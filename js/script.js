e70dace7931525f1df0679a6a7b44a26085c3488
Yousuf Ali Shafin <118128525+yousuf-safin@users.noreply.github.com>
2023-04-29 15:06:31 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,30 @@
+
+
+$(document).ready(function(){ 
+
+    // particle
+$('#example').polygonizr();
+// preloadinator  
+ $('#js-preloader').preloadinator({
+  animation: 'fadeOut',
+  animationDuration: 4000
+});
+
+
+     var mixer = mixitup('#hello', {
+    
+    animation: {
+        duration: 300
+    }
+});
+    
+
+    
+
+});
+
+
+
+
+
+
